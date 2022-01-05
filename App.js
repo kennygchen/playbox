f27251f1855a245acbca608b70fc4f652d563662
Kenny Chen <kennygchen@yahoo.com>
2022-01-04 22:36:00 -0800

    change text and background color

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: '#fff'}} >Open up App.js to start working on your app!</Text>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
@@ -12,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#168',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
