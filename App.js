69ae421e9976037c1d899cda4c688a719574bf5b
Ruoqi Huang <huangruoqi374@gmail.com>
2022-01-04 22:37:20 -0800

    third commit

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{color: '#fff'}} >Open up App.js to start working on your app!</Text>
+      <Text style={{color: '#fff'}} >Open up App.js to start working on your app!</Text>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
