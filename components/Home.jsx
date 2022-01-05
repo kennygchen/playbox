ef0f40490fee0b61a9fcba4d9adc665e07912d99
Ruoqi Huang <huangruoqi374@gmail.com>
2022-01-05 12:07:19 -0800

    changed container styles and items in Home

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,24 +1,54 @@
 import * as React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Dimensions } from 'react-native';
 
-
-
-export default function Home({route, navigation}) {
+function Item({name, nav}) {
 	return (
-		<SafeAreaView style={styles.container}>
-			<Text>{route.params.p} props {route.params.b}</Text>
-			<Button title='Go to Blank' onPress={() => navigation.navigate('Blank')}></Button>
-		</SafeAreaView>
+		<View style={styles.item}>
+			<Button title={name} onPress={() => nav.navigate('Blank')}></Button>
+		</View>
 	)
 }
 
 
+export default function Home({route, navigation}) {
+	const arr = [1,2,3,4,5,6,7];
+	return (
+		<SafeAreaView style={{flex:1, backgroundColor: '#000'}}>
+			<SafeAreaView style={styles.outContainer}>
+				<SafeAreaView style={styles.container}>
+					{
+						arr.map((e, i) => {
+							return (
+								<Item name={e+''} key={i} nav={navigation}></Item>
+							);
+						})
+					}
+				</SafeAreaView>
+			</SafeAreaView>
+		</SafeAreaView>
+	)
+}
+
+const {height, width} = Dimensions.get('screen');
+
+
 const styles = StyleSheet.create({
+	outContainer:{
+		flex:1,
+		margin: width/26,
+    backgroundColor: '#000',
+	},
   container: {
-    flex: 1,
+		flex:1,
 		flexDirection: 'row',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+		flexWrap: 'wrap',
   },
+	item: {
+		width: width/13*3,
+		height: width/13*3,
+		justifyContent: 'center',
+		alignItems: 'center',
+    backgroundColor: '#ddd',
+		borderWidth: 1,
+	}
 });
