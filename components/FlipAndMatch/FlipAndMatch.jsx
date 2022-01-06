e92a910da094a6e6037186ef31b80cad08c72e53
Kenny Chen <kennygchen@yahoo.com>
2022-01-06 02:12:01 -0800

    Included card images and implemented shuffled card array

--- components/FlipAndMatch/FlipAndMatch.jsx
+++ components/FlipAndMatch/FlipAndMatch.jsx
@@ -1,11 +1,34 @@
 import * as React from 'react'
-import { StyleSheet, Button, Text, SafeAreaView, View } from 'react-native'
+import { StyleSheet, Button, Text, SafeAreaView, View, Image } from 'react-native'
+
+const cardImage = [
+	{"src": "../img/chrome.png"},
+	{"src": "../img/edge.png"},
+	{"src": "../img/firefox.png"},
+	{"src": "../img/ie.png"},
+	{"src": "../img/opera.png"},
+	{"src": "../img/safari.png"},
+]
 
 export default function FlipAndMatch({navigation}) {
+	const [flip, setFlip] = React.useState(0)
+	const [cards, setCards] = React.useState([])
+
+	const shuffle = () =>{
+		const shuffled = [...cardImage, ...cardImage]
+			.sort(() => 0.5 - Math.random())
+			.map((card) => ({...card, id: Math.random()}))
+
+		setCards(shuffled)
+		setFlip(0)
+	}
+
+	console.log(cards, flip)
+
 	return (
 		<SafeAreaView style={style.container}>
 			<Text style={style.titleText}>Flip And Match</Text>
-			<Text>Card flipping game!!!</Text>
+			<Button title='New Game' onPress={shuffle}></Button>
 			<Button title='Back' onPress={() => navigation.navigate('Home')}></Button>
 		</SafeAreaView>
 	)
